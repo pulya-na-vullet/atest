078a89c3482e048eb960fb99da372bf9e703a2ae
Холина Наталья Олеговна <NKholina@alfabank.ru>
2026-07-09 14:24:10 +0300

    Добавлены UFR автотесты получения страховых программ

--- src/main/java/ru/alfabank/configs/Endpoints.java
+++ src/main/java/ru/alfabank/configs/Endpoints.java
@@ -15,4 +15,7 @@ public final class Endpoints {
     public static final String CONTRACT_PROGRAMS =
             "/corp-ncins-acc-gateway/secure/" +
                     "corp-ncins-acc-corp-ncins-acc-api/v1/ins-contracts";
+
+    public static final String UFR_INSURANCE_PROGRAMS =
+            "/v1/ins-programs";
 }
